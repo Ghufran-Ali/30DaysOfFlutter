3c51f3ad327ae80a04d22e7ad0b6c36384118821
Ghufran-Ali <ghufranali803@gmail.com>
2024-08-08 02:43:49 +0500

    Day3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "codepur";
+  //final int days = 30;
+  //final String name = "codepur";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Medical Kiosk"),
-        backgroundColor: Colors.blue,
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 253, 148, 11),
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          "IoT Based $days Medical Kiosk $name",
-          style: const TextStyle(fontSize: 26),
+          "IoT Based Medical Kiosk",
+          style: TextStyle(fontSize: 26, color: Colors.black),
         ),
       ),
       drawer: const Drawer(),
+      backgroundColor: const Color.fromARGB(255, 247, 239, 173),
     );
   }
 }
